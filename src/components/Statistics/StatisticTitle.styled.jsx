5ee0d13a185b59ac651c9c567cced2a9b93c129d
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2022-09-19 17:36:36 +0300

    change css

--- src/components/Statistics/StatisticTitle.styled.jsx
+++ src/components/Statistics/StatisticTitle.styled.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const StatTitle = styled.h2`
+    text-align: center;
     font-size: 30px;
-    color: red;
+    color: rgb(0, 0, 0, 0.5);
 `;
--- src/components/Statistics/StatisticWrapper.styled.jsx
+++ src/components/Statistics/StatisticWrapper.styled.jsx
@@ -3,7 +3,9 @@ import styled from '@emotion/styled';
 export const StatisticWrapperList = styled.ul`
     display: flex;
     justify-content: space-between;
-    padding: 10px 20px;
+    /* padding: 10px 20px; */
+    padding-right: 20px;
+    padding-left: 20px;
     list-style: none;
     background-color: rgb(0, 128, 0);
 `;
@@ -12,6 +14,12 @@ export const StatisticWrapperItem = styled.li`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    &:not(:last-child) {
+        padding-right: 30px;
+        border-right: 1px solid black;
+    }
 `;
 
 export const StatisticWrapperLabel = styled.span`
